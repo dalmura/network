2ff06125471c5821e8fce14c890c39fc29f3e18b
Michael Robbins <michael+github@dalmura.com.au>
2023-02-22 22:56:33 +1100

    Adding tld definitions into global

--- sites/global/terraform/dns.tf
+++ sites/global/terraform/dns.tf
@@ -1,4 +1,9 @@
-resource "aws_route53_zone" "global" {
+resource "aws_route53_zone" "global_au" {
   name    = "dalmura.au"
-  comment = "Dalmura Global Domain"
+  comment = "Dalmura Global AU Domain"
+}
+
+resource "aws_route53_zone" "global_cloud" {
+  name    = "dalmura.cloud"
+  comment = "Dalmura Global Cloud Domain"
 }
