eab9acc2482c4a5623ec45fa7b69f60442c3aaf5
Michael Robbins <michael+github@dalmura.com.au>
2023-08-20 23:04:57 +1000

    Adding in tf support for all management devices under the site

--- sites/indigo/terraform/dns.tf
+++ sites/indigo/terraform/dns.tf
@@ -1,6 +1,12 @@
+locals {
+  networks = yamldecode(file("${path.root}/../networks.yml"))
+
+  management_devices = local.networks["networks"]["MANAGEMENT"]["subranges"]["static"]["allocations"]
+}
+
 resource "aws_route53_record" "site_au" {
   zone_id = data.aws_route53_zone.global_au.zone_id
-  name    = "indigo.${data.aws_route53_zone.global_au.name}"
+  name    = "${local.site_name}.${data.aws_route53_zone.global_au.name}"
   type    = "CNAME"
   ttl     = "300"
   records = ["ec190e725191.sn.mynetname.net"]
@@ -8,8 +14,18 @@ resource "aws_route53_record" "site_au" {
 
 resource "aws_route53_record" "site_cloud" {
   zone_id = data.aws_route53_zone.global_cloud.zone_id
-  name    = "indigo.${data.aws_route53_zone.global_cloud.name}"
+  name    = "${local.site_name}.${data.aws_route53_zone.global_cloud.name}"
   type    = "CNAME"
   ttl     = "300"
   records = ["ec190e725191.sn.mynetname.net"]
+}
+
+resource "aws_route53_record" "site_management_devices" {
+  for_each = local.management_devices
+
+  zone_id = data.aws_route53_zone.global_cloud.zone_id
+  name    = "${trimprefix(each.value, local.site_device_prefix)}.${aws_route53_record.site_cloud.fqdn}"
+  type    = "A"
+  ttl     = "300"
+  records = [each.key]
 }
---
+++ sites/indigo/terraform/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  site_name          = "indigo"
+  site_dydns         = "ec190e725191.sn.mynetname.net"
+  site_device_prefix = "dal-${local.site_name}-"
+}
