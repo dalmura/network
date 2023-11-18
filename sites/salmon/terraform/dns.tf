d65d28a166b3ff8b4337d17d5644e084e9a6db06
Michael Robbins <michael+github@dalmura.com.au>
2023-11-18 14:56:11 +1100

    Adding in IOT INTERNET devices to DNS as well

--- sites/salmon/terraform/dns.tf
+++ sites/salmon/terraform/dns.tf
@@ -3,6 +3,9 @@ locals {
 
   # All devices including the gateway
   management_devices = local.networks["networks"]["MANAGEMENT"]["subranges"]["static"]["allocations"]
+
+  # Filter out the gateway
+  iot_internet_devices = {for k, v in local.networks["networks"]["IOT_INTERNET"]["subranges"]["static"]["allocations"]: k => v if v != "${local.site_device_prefix}fw-0"}
 }
 
 resource "aws_route53_record" "site_au" {
@@ -23,6 +26,16 @@ resource "aws_route53_record" "site_cloud" {
 
 resource "aws_route53_record" "site_management_devices" {
   for_each = local.management_devices
+
+  zone_id = data.aws_route53_zone.global_cloud.zone_id
+  name    = "${trimprefix(each.value, local.site_device_prefix)}.${aws_route53_record.site_cloud.fqdn}"
+  type    = "A"
+  ttl     = "300"
+  records = [each.key]
+}
+
+resource "aws_route53_record" "site_iot_internet_devices" {
+  for_each = local.iot_internet_devices
 
   zone_id = data.aws_route53_zone.global_cloud.zone_id
   name    = "${trimprefix(each.value, local.site_device_prefix)}.${aws_route53_record.site_cloud.fqdn}"
