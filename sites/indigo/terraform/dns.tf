619b41c1570fe831a81a851831a6c82f93188658
Michael Robbins <michael+github@dalmura.com.au>
2023-12-11 20:26:05 +1100

    Support loading the secrets config and bring in an optional dns cloud entries section

--- sites/indigo/terraform/dns.tf
+++ sites/indigo/terraform/dns.tf
@@ -1,8 +1,12 @@
 locals {
   networks = yamldecode(file("${path.root}/../networks.yaml"))
+  secrets = yamldecode(file("${path.root}/../secrets.yaml"))
 
   # All devices including the gateway
   management_devices = local.networks["networks"]["MANAGEMENT"]["subranges"]["static"]["allocations"]
+
+  # Optional cloud entries
+  cloud_entries = lookup(lookup(local.secrets, "dns", {}), "cloud", {})
 }
 
 resource "aws_route53_record" "site_au" {
@@ -29,4 +33,14 @@ resource "aws_route53_record" "site_management_devices" {
   type    = "A"
   ttl     = "300"
   records = [each.key]
+}
+
+resource "aws_route53_record" "site_cloud_entries" {
+  for_each = local.cloud_entries
+
+  zone_id = data.aws_route53_zone.global_cloud.zone_id
+  name    = "${each.key}.${aws_route53_record.site_cloud.fqdn}"
+  type    = "A"
+  ttl     = "300"
+  records = [each.value]
 }
